7d99d47d1faac17d66ec5bc475bea8de50e8a927
Lewis Lucas <dajia1701@gmail.com>
2024-12-08 14:36:21 +0800

    feat: pass flags configuration to MinecraftProxy reload

--- src/index.ts
+++ src/index.ts
@@ -102,6 +102,7 @@ async function main() {
 				logger.level = config.logger.level
 				minecraftProxy.reload({
 					inbound: config.inbound,
+					flags: config.flags,
 				})
 				await pluginLoader.loadPlugins('./plugins', config)
 
