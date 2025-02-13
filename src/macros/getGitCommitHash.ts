12e44d9d84c57e8e2c516aeb3b44fbb83e7e045c
Ikaleio <dajia1701@gmail.com>
2025-02-13 20:01:29 +0800

    release: v1.1.2

--- src/macros/getGitCommitHash.ts
+++ src/macros/getGitCommitHash.ts
@@ -1,11 +1,8 @@
 export async function getGitCommitHash() {
-	return process.env.COMMIT_HASH?.toString().trim().substring(0, 7) || 'UNKNOWN'
-	// 下面这个（更优的）实现会在 Windows 环境下造成 Bun 崩溃
-	// 在 Bun 修好之前我们先读环境变量
-	// try {
-	// 	return (await Bun.$`git rev-parse HEAD`).toString().trim().substring(0, 7)
-	// } catch (error) {
-	// 	console.error('Error in getGitCommitHash', error)
-	// 	return 'UNKNOWN'
-	// }
+	try {
+		return (await Bun.$`git rev-parse HEAD`).toString().trim().substring(0, 7)
+	} catch (error) {
+		console.error('Error in getGitCommitHash', error)
+		return 'UNKNOWN'
+	}
 }
